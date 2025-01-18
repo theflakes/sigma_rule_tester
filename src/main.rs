60e7989a7338de555230003e08437f3f041ce840
Brian Kellogg <theflakes@gmail.com>
2025-01-18 17:30:17 -0500

    Add time measurements

--- src/main.rs
+++ src/main.rs
@@ -1,39 +1,60 @@
-use std::{env, io::{self, Read}, process};
+use std::{env, io::{self, Read}, process, time::{Duration, Instant}};
 use serde::Serialize;
 use serde_json::Value;
 use sigma_rust::{Rule, rule_from_yaml, event_from_json};
 use walkdir::WalkDir;
 
+
+#[derive(Debug, Default, Serialize)]
+struct MatchedRules {
+    rule_title: String,
+    time_taken_micro: u128,
+}
+
 #[derive(Debug, Default, Serialize)]
 struct SigmaResults {
-    rule_count: usize,
-    rules: Vec<Value>,
+    time_taken_milli: u128,
+    time_taken_micro: u128,
+    matched_rule_count: usize,
+    matched_rules: Vec<MatchedRules>,
 }
 
-fn apply_sigma_rules(log_entry: &Value, rules: &Vec<Rule>) -> Vec<Value> {
-    let mut matched_rules = Vec::new();
+
+fn apply_sigma_rules(log_entry: &Value, rules: &[Rule]) -> (Vec<MatchedRules>, Duration) {
     let event = event_from_json(&log_entry.to_string()).unwrap();
+    
+    let mut matched_rules = Vec::new();
+    let mut total_duration = Duration::new(0, 0);
 
     for rule in rules {
+        let start = Instant::now();
+
         if rule.is_match(&event) {
-            matched_rules.push(Value::String(rule.title.clone()));
+            let duration = start.elapsed();
+            matched_rules.push(MatchedRules {
+                rule_title: rule.title.clone(),
+                time_taken_micro: duration.as_micros(),
+            });
+            total_duration += duration;
         }
     }
-
-    return matched_rules
+    // matched_rules.sort_by_key(|matched_rule| &matched_rule.name);
+    return (matched_rules, total_duration);
 }
 
-fn print_results(total: i32, successful: i32, failed: i32, errors: &Vec<String>, print_errors: bool) {
+fn print_results(total: i32, successful: i32, failed: i32, errors: &Vec<String>, print_errors: bool, duration: Duration) {
     if print_errors {
         println!(
             "Rule Load Summary:\n\
                - Total: {}\n\
                - Successful: {}\n\
                - Failed: {}\n\
+               - Load time: {:?}\n\
                - Errors: {:?}",
             total,
             successful,
             failed,
+            duration,
             errors,
         );
         return
@@ -42,11 +63,22 @@ fn print_results(total: i32, successful: i32, failed: i32, errors: &Vec<String>,
         "Rule Load Summary:\n\
            - Total: {}\n\
            - Successful: {}\n\
-           - Failed: {}",
+           - Failed: {}\n\
+           - Load time: {:?}",
         total,
         successful,
         failed,
+        duration,
     );
+}
+
+fn read_file_to_string(path: &std::path::Path) -> Result<String, String> {
+    let mut file = std::fs::File::open(path)
+        .map_err(|e| format!("Unable to open file {:?}: {}", path, e))?;
+    let mut contents = String::new();
+    file.read_to_string(&mut contents)
+        .map_err(|e| format!("Unable to read file {:?}: {}", path, e))?;
+    Ok(contents)
 }
 
 fn load_rules(rules_dir: &str, print_errors: bool) -> Result<Vec<Rule>, String> {
@@ -56,6 +88,7 @@ fn load_rules(rules_dir: &str, print_errors: bool) -> Result<Vec<Rule>, String> 
     let mut rules = vec![];
     let mut errors = vec![];
 
+    let start = Instant::now();
     for entry in WalkDir::new(rules_dir).into_iter().filter_map(|e| e.ok()) {
         let extension = match entry.path().extension().and_then(|s| s.to_str()) {
             Some(ext) => ext.to_lowercase(),
@@ -82,17 +115,9 @@ fn load_rules(rules_dir: &str, print_errors: bool) -> Result<Vec<Rule>, String> 
             };
         }
     }
-    print_results(total, successful, failed, &errors, print_errors);
+    let duration = start.elapsed();
+    print_results(total, successful, failed, &errors, print_errors, duration);
     Ok(rules)
-}
-
-fn read_file_to_string(path: &std::path::Path) -> Result<String, String> {
-    let mut file = std::fs::File::open(path)
-        .map_err(|e| format!("Unable to open file {:?}: {}", path, e))?;
-    let mut contents = String::new();
-    file.read_to_string(&mut contents)
-        .map_err(|e| format!("Unable to read file {:?}: {}", path, e))?;
-    Ok(contents)
 }
 
 fn main() -> io::Result<()>  {
@@ -105,13 +130,13 @@ fn main() -> io::Result<()>  {
     let reader = std::io::BufReader::new(file); 
     let log = serde_json::from_reader(reader).unwrap();
     let rules = load_rules(&rules, print_errors).unwrap();
-    let mut matched_rules = apply_sigma_rules(&log, &rules);
-    matched_rules.sort_by(|a, b| { 
-        a.as_str().unwrap_or("").cmp(&b.as_str().unwrap_or(""))
-    });
+    let (mut matched_rules, duration) = apply_sigma_rules(&log, &rules);
+    matched_rules.sort_by_key(|rule| rule.rule_title.clone());
     let sigma_results = SigmaResults {
-        rule_count: matched_rules.len(), 
-        rules: matched_rules,
+        time_taken_milli: duration.as_millis(),
+        time_taken_micro: duration.as_micros(),
+        matched_rule_count: matched_rules.len(),
+        matched_rules: matched_rules,
     };
     let pretty_printed = serde_json::to_string_pretty(&sigma_results).unwrap();
     println!("\nResults:\n{}", pretty_printed);
